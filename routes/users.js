40b9dab0a85014010b41cf2f30ebb535dc0acebf
Mohammad Arham <arhammusheer@gmail.com>
2020-09-28 12:02:37 +0530

    verification done

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
+var mailer = require("../email");
+const jwt = require("jsonwebtoken");
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
@@ -14,14 +16,53 @@ router.get("/", (req, res, next) => {
 router.get("/admin", async (req, res, next) => {
 	if (req.user) {
 		if (req.user.isAdmin) {
-			var userMap = {};
+			var userMap = [];
 			await User.find({}, (err, users) => {
 				userMap = users;
 			});
-			return res.render("admin_index", { userMap: userMap });
+			return res.render("admin_index", { userMap: JSON.stringify(userMap) });
 		}
 	}
 	return res.redirect("/");
+});
+
+router.get("/send-email", (req, res, next) => {
+	token = jwt.sign({ userid: req.user._id }, process.env.JWT_SECRET, {
+		expiresIn: "1h",
+	});
+	mailer(req.user.umassEmail, `${req.get("host")}/u/verify/${token}`);
+	res.send("Sent :)");
+});
+
+router.get("/verify/:token", async (req, res, next) => {
+	token = req.params.token;
+	data = await jwt.verify(token, process.env.JWT_SECRET);
+	userExists = await User.exists({ _id: data.userid });
+	console.log(data);
+	if (userExists) {
+		User.findByIdAndUpdate(data.userid, { umassVerified: true }, function (
+			err,
+			user
+		) {
+			req.user.umassVerified = true;
+			return res.render("verified");
+		});
+	} else {
+		return res.render("not_verified");
+	}
+});
+
+router.post("/update-email", async (req, res, next) => {
+	await User.findByIdAndUpdate(
+		req.user._id,
+		{
+			umassEmail: req.body.umassEmail,
+		},
+		function (err, docs) {
+			if (!err) req.user.umassEmail = req.body.umassEmail;
+		}
+	);
+	res.redirect("/");
 });
 
 module.exports = router;
